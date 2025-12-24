9d7dc92719b7f0fd7c9450857060414cd723bda2
gaurav <lakhera2525@gmail.com>
2025-12-24 23:04:25 +0530

    update origin

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://127.0.0.1:3000',
+      'https://eri-astronfinancials.netlify.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
